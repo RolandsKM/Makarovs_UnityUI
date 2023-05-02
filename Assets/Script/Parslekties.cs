726d4d7017c20964073ffe0740fed556ae49c0fa
Makarovs <rolandsmakarovs264@gmail.com>
2023-05-02 10:50:03 +0300

    Ainas

---
+++ Assets/Script/Parslekties.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Parslekties : MonoBehaviour {
+
+    public void uzSakumu()
+    {
+        SceneManager.LoadScene("Charecter", LoadSceneMode.Single);
+
+    }
+    public void Atpakal()
+    {
+        Application.Quit();
+    }
+    public void uzUI()
+    {
+        SceneManager.LoadScene("Makarovs_UnityUI", LoadSceneMode.Single);
+
+    }
+}
