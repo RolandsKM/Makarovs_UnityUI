c049ce2de36ff3c00ae52beeb35d4ec49fc9030c
Makarovs <rolandsmakarovs264@gmail.com>
2023-05-06 17:47:20 +0300

    Avatara Sievietes/Viriešu Izvēle

---
+++ Assets/Script/AteluParadisana.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AteluParadisana : MonoBehaviour {
+	public Toggle toggle, toggle2;
+	public GameObject Male;
+	public GameObject Female; 
+	public GameObject Mati;
+	public GameObject Mati2;
+	private Image image;
+	private Image image2;
+	private void Start()
+	{
+		image = Male.GetComponent<Image> ();
+		image2 = Female.GetComponent<Image> ();
+		image = Mati.GetComponent<Image> ();
+		image2 = Mati2.GetComponent<Image> ();
+		toggle.onValueChanged.AddListener (OnToggleValueChanged);
+		toggle2.onValueChanged.AddListener (OnToggleValueChanged2);
+
+	}
+	private void OnToggleValueChanged(bool value){
+		Male.SetActive (value);
+		Mati.SetActive (value);
+	}
+
+	private void OnToggleValueChanged2(bool value){
+		Female.SetActive (value);
+		Mati2.SetActive (value);
+	}
+
+}
---
+++ Assets/Script/Ievade.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Ievade : MonoBehaviour {
+
+	public string teksts;
+	public int Vecums;
+	public GameObject IevadesLauks;
+	public GameObject IevadesLauksVecums;
+	public GameObject TekstaAttelosana;
+
+	int indekss;
+	public void UzglabaTekstu()
+	{
+		
+		teksts = IevadesLauks.GetComponent<InputField>().text;
+		int.TryParse(IevadesLauksVecums.GetComponent<InputField>().text, out Vecums);
+		TekstaAttelosana.GetComponent<Text>().text = "Spélétájs " + teksts + " Ir " + Vecums.ToString() + " Gadus Vecs!";
+
+
+	}
+
+}
