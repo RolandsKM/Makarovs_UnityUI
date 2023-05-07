efcfc1c08342b2ab6764fcacf3f51ac11c9c8eb5
Makarovs <rolandsmakarovs264@gmail.com>
2023-05-07 12:12:44 +0300

    Pogu sakartošana/dropdown Pielikšana/Nepabeikts sliders

---
+++ Assets/Script/IzmeraUnPlatumaMaiņa.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class IzmeraUnPlatumaMaiņa : MonoBehaviour {
+	public GameObject mainigaisAttels;
+	public GameObject MainigasiG;
+	public GameObject sliderisIzmes;
+	public Sprite[] AtteluMasivs;
+	public void mainitlielumu()
+	{
+		float pasreizejaVertiba = sliderisIzmes.GetComponent<Slider>().value;
+		mainigaisAttels.transform.localScale = new Vector2(1f*pasreizejaVertiba, 1f*pasreizejaVertiba);
+	}
+
+
+
+	public void izkritosais(int skaitlis)
+	{
+		if (skaitlis == 0)
+
+			MainigasiG.GetComponent<Image>().sprite = AtteluMasivs[0];
+		else if (skaitlis == 1)
+			MainigasiG.GetComponent<Image>().sprite = AtteluMasivs[1];
+		else if (skaitlis == 2)
+			MainigasiG.GetComponent<Image>().sprite = AtteluMasivs[2];
+		else
+			Debug.Log("Nev piesaistītais attels");
+	}
+
+}
